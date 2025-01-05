196e383a80bbce42c78f0f18fcf90830c7535788
werdani <ammaryasser554zz@gmail.com>
2025-01-06 01:01:05 +0200

    feat: register models in Django admin for easy management

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import UploadedPDF, UploadedImage
+
+admin.site.register(UploadedPDF)
+admin.site.register(UploadedImage)
